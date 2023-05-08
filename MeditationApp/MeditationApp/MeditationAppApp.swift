73922f46d674ee018c0eb9422e7651152b1c72d1
Pericica Iustin <pericicaiustin@gmail.com>
2023-05-08 19:02:06 +0300

    project setup

---
+++ MeditationApp/MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Iustin Pericica on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
